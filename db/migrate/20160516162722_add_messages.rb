1feed0a4ae72e172e343645c6a530f110371d4df
Adriana Molano <molanoa@delmar.ca>
2016-05-16 18:42:48 +0000

    <commit message>

--- db/migrate/20160516162722_add_messages.rb
+++ db/migrate/20160516162722_add_messages.rb
@@ -1,9 +1,10 @@
 class AddMessages < ActiveRecord::Migration
     def change
    create_table :messages do |t|
-    # t.string :title
     t.string :author
     t.string :content
+    t.string :url
+    t.timestamps
     end
   end
 end
