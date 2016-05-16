8a4954f2ce62a04f447be547959c607ff29f3150
Adriana Molano <molanoa@delmar.ca>
2016-05-16 18:15:24 +0000

    <commit message>

--- app/actions.rb
+++ app/actions.rb
@@ -8,12 +8,26 @@ get '/messages' do
   erb :'messages/index'
 end
 
+get '/messages/new' do
+  @message = Message.new
+  erb :'messages/new'
+end
+
+get '/messages/:id' do
+  @message = Message.find params[:id]
+  erb :'messages/show'
+end
+
 post '/messages' do
-   @message = Message.new(
-    title:   params[:title],
-     content: params[:content],
-     author:  params[:author]
-   )
-   @message.save
-   redirect '/messages'
- end
+  @message = Message.new(
+    # title:   params[:title],
+    content: params[:content],
+    author:  params[:author],
+    url:  params[:url]
+  )
+  if @message.save
+    redirect '/messages'
+  else
+    erb :'messages/new'
+  end
+end
